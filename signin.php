253d3a0c806c9109deebdbb02e84eea85b28be7f
Chand Rahman <chand.stack@gmail.com>
2024-03-21 15:23:19 +0000

    Feat: signin page added with feature

--- signin.php
+++ signin.php
@@ -1,3 +1,13 @@
+<?php
+require 'config/constants.php';
+
+$username_email = $_SESSION['signin-data']['username_email'] ?? null;
+$password = $_SESSION['signin-data']['password'] ?? null;
+
+unset($_SESSION['signin-data']);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,13 +27,32 @@
 <section class="form_section">
 <div class="container form_section-container">
 <h2>Sign In</h2>
-<div class="alert_message success">
-<p>This is an success message</p>
+
+<?php if (isset($_SESSION['signup-success'])) : ?>
+    <div class="alert_message success">
+    <p>
+    <?= $_SESSION['signup-success'];
+    unset($_SESSION['signup-success']);
+    ?>
+</p>
 </div>
-<form>
-    <input type="text" placeholder="Username or Email">
-    <input type="password" placeholder="Password">
-     <button class="btn" type="submit">Sign In</button>
+
+<?php elseif (isset($_SESSION['signin'])) : ?>
+
+    <div class="alert_message error">
+    <p>
+    <?= $_SESSION['signin'];
+    unset($_SESSION['signin']);
+    ?>
+</p>
+</div>
+
+<?php endif; ?>
+
+<form action="<?= ROOT_URL ?>signin-logic.php" method="post">
+    <input type="text" value="<?= $username_email ?>" name="username_email" placeholder="Username or Email">
+    <input type="password" value="<?= $password ?>" name="password" placeholder="Password">
+     <button class="btn" name="submit" type="submit">Sign In</button>
      <small>Don't have an account? <a href="./signup.php">Sign Up</a></small>
 </form>
 </div>
