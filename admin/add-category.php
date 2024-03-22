f2f903f3b7fbaaede51b8330e2c3e5bf6c54dcb8
Chand Rahman <chand.stack@gmail.com>
2024-03-22 16:58:53 +0000

    Feat: add category feature added

--- admin/add-category.php
+++ admin/add-category.php
@@ -1,16 +1,29 @@
 <?php
-include 'partials/header.php'
+include 'partials/header.php';
+
+// get back form data in invalid
+$title = $_SESSION['add-category-data']['title'] ?? null;
+$description = $_SESSION['add-category-data']['description'] ?? null;
+
+unset($_SESSION['add-category-data']);
 ?>
+
 <section class="form_section">
 <div class="container form_section-container">
 <h2>Add Category</h2>
-<div class="alert_message error">
-<p>This is an error message</p>
+<?php if(isset($_SESSION['add-category'])) : ?>
+    <div class="alert_message error">
+<p>
+    <?= $_SESSION['add-category'];
+    unset($_SESSION['add-category'])
+    ?>
+</p>
 </div>
-<form>
-    <input type="text" placeholder="Title">
-     <textarea rows="4" placeholder="Description"></textarea>
-     <button class="btn" type="submit">Add Category</button>
+    <?php endif ?>
+<form action="<?= ROOT_URL ?>admin/add-category-logic.php" method="post">
+    <input type="text" value="<?= $title ?>" name="title" placeholder="Title">
+     <textarea rows="4" value="<?= $description ?>" name="description" placeholder="Description"></textarea>
+     <button class="btn" name="submit" type="submit">Add Category</button>
 </form>
 </div>
 </section>
